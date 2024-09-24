e1e8065159a161f28b68aebb66add5614c5e6cfe
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2024-09-24 10:38:58 -0300

    feat: Implementação e configuração do plugin @stencil/sass.

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,26 +1,28 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'ph-stencil-js',
+  plugins: [sass()],
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader',
+      esmLoaderPath: '../loader'
     },
     {
       type: 'dist-custom-elements',
       customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false,
+      externalRuntime: false
     },
     {
-      type: 'docs-readme',
+      type: 'docs-readme'
     },
     {
       type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+      serviceWorker: null // disable service workers
+    }
   ],
   testing: {
-    browserHeadless: "new",
-  },
+    browserHeadless: 'new'
+  }
 };
