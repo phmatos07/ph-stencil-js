8f4a11cfc8f7b988e21f791931cd1ddff1ce0112
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2025-02-19 09:57:26 -0300

    feat: Implementação dos processos esconder o modal.
    Author: Phelipe Matos (PHMATOS07)

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { Component, h, JSX, Prop } from '@stencil/core';
+import { Component, h, JSX, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'phsj-modal',
@@ -9,34 +9,47 @@ export class Modal {
   @Prop()
   title!: string;
 
+  @State()
   private isDisplayed = true;
 
+  @State()
+  private isHidden = false;
+
   private close(): void {
-    console.log('close');
-    this.isDisplayed = !this.isDisplayed;
+    const TIME_TO_HIDE = 1000;
+    this.isHidden = true;
+
+    setTimeout(() => {
+      this.isDisplayed = false;
+    }, TIME_TO_HIDE);
   }
 
   render(): JSX.Element | null {
-    return (
+    return this.isDisplayed ? (
       <section class="modal">
         <div
           class={{
             modal__shadow: true,
-            'modal--visible': this.isDisplayed
+            'modal__shadow--display': !this.isHidden,
+            'modal__shadow--to-hide': this.isHidden
           }}
         >
-          <div class="modal__content">
+          <div
+            class={{
+              modal__content: true,
+              'modal__content--invisible': this.isHidden
+            }}
+          >
             <header class="modal__header">
               <h1>{this.title}</h1>
               <button onClick={() => this.close()}>X</button>
             </header>
-
             <main class="modal__main">
               <slot></slot>
             </main>
           </div>
         </div>
       </section>
-    );
+    ) : null;
   }
 }
